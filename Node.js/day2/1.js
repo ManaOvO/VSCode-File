8f29c8e9c100e300d31cc97bcb52ac8eedd20029
Mana <285283473@qq.com>
2023-05-05 20:00:32 +0800

    5.5前端学习

---
+++ Node.js/day2/1.js
@@ -0,0 +1,4 @@
+import $ from "jquery"
+const moment = require('moment')
+const dt = moment().format('YYYY-MM-DD HH:mm:ss')
+console.log(dt)
---
+++ Node.js/day2/mana-tool/README.md
@@ -0,0 +1,3 @@
+package.json  包的管理配置文件
+index.js  包的入口文件
+README.md 包的说明文档
